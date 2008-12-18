1c0975cd1e9ad4a76eb0345fb419b394eaec8380
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2008-12-17 20:13:02 -0500

    Added some Application code. Compiler-related interactions are only vaguely present.

--- lib/gymnast/application.rb
+++ lib/gymnast/application.rb
@@ -1,6 +1,14 @@
 module Gymnast
   class Application
-    def initialize(a)
+    attr_reader :app_file
+
+    def initialize(app_file, flex_project)
+      @app_file = app_file
+      @flex_project = flex_project
+    end
+
+    def compiler_line
+      @flex_project.compiler_line
     end
   end
 end
--- spec/gymnast/application_spec.rb
+++ spec/gymnast/application_spec.rb
@@ -1,10 +1,26 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 require 'gymnast/application'
+require 'gymnast/flex_project'
+
 include Gymnast
 
 describe Application do
   before(:each) do
-    @application = Application.new 'FiveCylons.mxml', mock(FlexProject)
+    @fp_mock = mock(FlexProject)
+
+    @it = Application.new 'FiveCylons.mxml', @fp_mock
+  end
+
+  it 'should provide access to its mxml file' do
+    @it.app_file.should == 'FiveCylons.mxml'
+  end
+
+  it 'should talk to its Flex project when providing its compiler line' do
+    pending 'should be clearer'
+
+    @fp_mock.should_receive(:compiler_line)
+
+    @it.compiler_line
   end
 end
